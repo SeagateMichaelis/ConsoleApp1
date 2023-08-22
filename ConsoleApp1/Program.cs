112406d33328bfc1485cf226cca3b67111b9da48
2022101754 <2022101754@LAB2-008>
2023-08-22 20:09:59 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using ConsoleApp1;
+/*
+int num1 = 20;
+int num2 = 11, result;
+
+
+
+Somador soma;
+soma = new Somador();
+
+result = soma.Soma(num1, num2);
+    Console.WriteLine(result);*/
+
+double metros = 20, result;
+
+Conversor conversor;
+conversor = new Conversor();
+/*
+result = conversor.MetrosMilimetros(metros);
+Console.WriteLine($"a medida {metros} metros corresponde a {result} milimetros");*/
+
+Console.WriteLine("A medida {0} metros corresponde
+
+
+
+
+
+
+
+
+
